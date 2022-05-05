79872b74e9676d7f427b34952c5aeed51d45563c
Ahmad Noor <99959745+Ahmad-ft-215@users.noreply.github.com>
2022-05-05 20:29:45 +0530

    storedevtool

--- src/store.js
+++ src/store.js
@@ -7,6 +7,7 @@ const rootReducer = combineReducers({
     prod:ProdReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
